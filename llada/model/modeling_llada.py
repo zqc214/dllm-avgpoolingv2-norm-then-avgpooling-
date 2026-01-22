cb9c6f25f5cf06b88cd03e66a989ae5b99bba30c
zqc <22447067@zju.edu.cn>
2026-01-22 15:06:00 +0800

    norm then avgpooling

--- llada/model/modeling_llada.py
+++ llada/model/modeling_llada.py
@@ -1696,3 +1696,4 @@ class LLaDAModelLM(PreTrainedModel):
 
 # Register the model so that it is available for transformer pipelines, auto-loading, etc.
 AutoModel.register(LLaDAConfig, LLaDAModelLM)
+
